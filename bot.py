b49e6a6b6ec6b3b5f8aea28cc796315677d37240
beng3r <82314309+beng3r@users.noreply.github.com>
2021-06-05 14:39:18 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,38 +1,36 @@
 import os
+import discord
+from discord.ext import commands,tasks
 from dotenv.main import load_dotenv
 from discord.ext import commands
+import youtube_dl
+import asyncio
 
 def main():
-    client = commands.Bot(command_prefix="?")
-
+    #intents = discord.Intents().all()
+    #client = discord.Client(intents=intents)
+    #bot = commands.Bot(command_prefix='!',intents=intents)
+    client = commands.Bot(command_prefix=".")
+    players = {}
     load_dotenv()
-
+    
     @client.event
     async def on_ready():
     
         print(f"{client.user.name} has connected to Discord.")
 
-    # @client.event
-    # async def on_message(message):
-    #     if (message.content.startswith("Hello")):
-    #         await message.channel.send(f"Hi {message.author.mention}!")
-
-    @client.command()
-    async def ping(ctx):
-        """Checks for a response from the bot"""
-        await ctx.send("Pong")
+    for folder in os.listdir("modules"):
+        if os.path.exists(os.path.join("modules", folder, "cog.py")):
+            client.load_extension(f"modules.{folder}.cog")
     
-    @client.command(pass_context = True)
-    async def join(ctx):
-        """Join Voice Channel"""
-        channel = ctx.author.voice.channel
-        await channel.connect()
-    @client.command(pass_context = True)
-    async def leave(ctx):
-        """Leave Voice """
-        await ctx.voice_client.disconnect()
 
     client.run(os.getenv("DISCORD_TOKEN"))
+
+
+
+
+
+
 
 if __name__ == '__main__':
     main()
