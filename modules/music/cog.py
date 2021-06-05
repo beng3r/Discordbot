2ff7181890d46df7d8fe289ed3ade8acc9197255
beng3r <82314309+beng3r@users.noreply.github.com>
2021-06-05 17:38:02 +0800

    Update cog.py

--- modules/music/cog.py
+++ modules/music/cog.py
@@ -27,9 +27,9 @@ ytdlopts = {
     'source_address': '0.0.0.0'  # ipv6 addresses cause issues sometimes
 }
 
-ffmpegopts = {
-    'before_options': '-nostdin',
-    'options': '-vn'
+ffmpeg_options = {
+    'options': '-vn',
+    "before_options": "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5"
 }
 
 ytdl = YoutubeDL(ytdlopts)
